e600b350e9a548d6e877c2f867d563b93e5905a6
otuoio <mariocoppo91@gmail.com>
2022-02-01 15:19:56 +0100

    add init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- Consegna: Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+    
+</body>
+</html>
